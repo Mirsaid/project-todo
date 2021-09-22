118a5239dee3a1bb3229f5b2a15bdc26e7106df6
Mirsaid Alizadeh <mirsaid.alizadeh@pasha-technology.com>
2021-09-22 14:22:39 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState,useRef } from "react"
+import TodoList from "./TodoList"
+
+const App = () =>{
+
+const [todo, settodo] = useState([])
+
+const inputval = useRef(null);
+
+const onFormSubmit = (e) => {
+    // `current` points to the mounted text input element
+    e.preventDefault()
+    let inpval = inputval.current.value;
+    inputval.current.value = ""
+    let arr = todo
+    arr.push(inpval)
+    settodo(arr)
+  };
+
+
+
+    return(
+        
+        
+        // <div className="ui container">
+        //     <div className="ui form">
+        //         <div className="field">
+        //             <form onSubmit={onFormSubmit}>
+        //                 <label>TASKS</label>
+                        
+        //                 <input type="text" placeholder="type here.." className="form-control"  ref={inputval} />
+        //                 {/* onChange={e => settodo(e.target.value)} */}
+        //             </form>
+        //        </div>
+        //     </div>
+        //             <div style={{marginTop:"20px", width:"100px"}}>
+        //     {todo.map((text,index) => <div key={`todoitem${index}`}>{text}</div>)} 
+        //             </div>           
+        // </div>
+        
+        <div>
+        <h1 className="text-center">TODOS</h1>
+        <div className="container" >
+            <div className="row">
+                <div className="col-12">
+                    <div className="form-group">
+                    <form onSubmit={onFormSubmit}>                
+                        <input type="text" placeholder="type here.." className="form-control"  ref={inputval} />
+                        
+                     </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+        {todo.map((text,index) => <div key={`todoitem${index}`}>{text}</div>)}
+        </div>
+        </div>
+    )
+
 }
 
-export default App;
+export default App
---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const TodoList = () =>{
+
+const[todoInput, settodoInput] = useState("");  
+const[fromform, setaslist] = useState("")
+
+
+
+
+
+
+
+
+}
+
+export default TodoList
